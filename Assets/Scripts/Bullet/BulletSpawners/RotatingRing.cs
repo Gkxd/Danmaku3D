77c4fb662c18700c0027e2c442d4edfdfc3afae2
Gkxd <dhua@andrew.cmu.edu>
2015-05-09 20:51:21 -0400

    Added basic bullet spawner

---
+++ Assets/Scripts/Bullet/BulletSpawners/RotatingRing.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotatingRing : _BulletSpawner {
+
+    public int amount;
+    public float bulletSpeed = 10;
+    public float bulletSpread = 360;
+    public float bulletLifetime = 10;
+
+    public Vector3 ringRotationAxis;
+    public float ringRotationAmount;
+    public float ringSpinAmount;
+
+    private Vector3 orthogonalAxis = Vector3.up;
+    private float angleOffset;
+
+    public override void spawnBullets() {
+        for (int i = 0; i < amount; i++) {
+            Vector3 direction = Quaternion.AngleAxis(bulletSpread * i / amount + angleOffset, orthogonalAxis) * Vector3.Cross(orthogonalAxis, ringRotationAxis);
+            StraightLineBullet bulletBehavior = new StraightLineBullet(bulletSpeed, direction);
+
+            spawnBullet(bulletPrefab, bulletBehavior, appearence, bulletLifetime);
+        }
+        orthogonalAxis = Quaternion.AngleAxis(ringRotationAmount, ringRotationAxis) * orthogonalAxis;
+        angleOffset += ringSpinAmount;
+    }
+
+}
--- Assets/Scripts/Bullet/BulletSpawners/_BulletSpawner.cs
+++ Assets/Scripts/Bullet/BulletSpawners/_BulletSpawner.cs
@@ -1,26 +1,49 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class BulletSpawn {
+public abstract class _BulletSpawner : MonoBehaviour {
+    
+    public GameObject bulletPrefab;
+    public BulletAppearence appearence;
+
+    public float delaySeconds;
+
+    void Start() {
+        init(delaySeconds);
+    }
+
+    private void init(float delaySeconds) {
+        StartCoroutine(spawnLoop(delaySeconds));
+    }
+
+    private IEnumerator spawnLoop(float delaySeconds) {
+        while (true) {
+            spawnBullets();
+            yield return new WaitForSeconds(delaySeconds);
+        }
+    }
+
+    public abstract void spawnBullets();
+
     public static void spawnBullet(GameObject bulletPrefab,
                                    _BulletBehavior behavior,
                                    Color color,
                                    Blending blendMode = Blending.Alpha,
                                    ColorBlending colorBlendMode = ColorBlending.Add,
                                    float lifeTime = 10) {
-
+        
         GameObject bullet = GameObject.Instantiate(bulletPrefab);
-
+        
         BulletVisual bulletVisual = bullet.GetComponent<BulletVisual>();
-
+        
         bulletVisual.appearence.color = color;
         bulletVisual.appearence.colorBlendMode = colorBlendMode;
         bulletVisual.appearence.blendMode = blendMode;
-
+        
         BulletUpdate bulletUpdate = bullet.AddComponent<BulletUpdate>();
         bulletUpdate.init(behavior, lifeTime);
     }
-
+    
     public static void spawnBullet(GameObject bulletPrefab,
                                    _BulletBehavior behavior,
                                    Blending blendMode = Blending.Alpha,
@@ -35,6 +58,20 @@ public class BulletSpawn {
         bulletVisual.appearence.colorBlendMode = colorBlendMode;
         bulletVisual.appearence.blendMode = blendMode;
         
+        BulletUpdate bulletUpdate = bullet.AddComponent<BulletUpdate>();
+        bulletUpdate.init(behavior, lifeTime);
+    }
+
+    public static void spawnBullet(GameObject bulletPrefab,
+                                   _BulletBehavior behavior,
+                                   BulletAppearence appearence,
+                                   float lifeTime) {
+        
+        GameObject bullet = GameObject.Instantiate(bulletPrefab);
+        
+        BulletVisual bulletVisual = bullet.GetComponent<BulletVisual>();
+        bulletVisual.appearence = appearence;
+
         BulletUpdate bulletUpdate = bullet.AddComponent<BulletUpdate>();
         bulletUpdate.init(behavior, lifeTime);
     }
